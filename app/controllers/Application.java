a47fc0167d12a56f9d981d185dd13ebeee4100d1
津崎 <g1244730@cse.kyoto-su.ac.jp>
2016-02-15 05:04:43 +0900

    アプリケーションを変更した

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,17 +1,33 @@
 package controllers;
 
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
-
-    public Result index() {
-        return ok(index.render(
-            "Your new application is ready.",
-            "My name is zakimoto",
-            "this page is sample!"));
+    
+    //フォーム内の内部クラス
+    public static class SampleForm{
+        public String message;
+        
+    //ルートにアクセスした際のアクション
+    public static Result index(){
+        return ok(index.render("何か書いて",new Form(SampleForm.class)));
+        }
+    
+    //sendにアクセスした際のアクション
+    public static Result send(){
+      Form<SampleForm> f =   form(SampleForm.class).bindFormRequest();
+      if(!f.haserrors()){
+      }
+      SampleForm data = f.get();
+      String msg = "you typed" + data.message;
+      retun ok(index.render(msg.f));
+    } else {
+        return badrequest(index.render("ERROR",form(SampleForm.class));
+        }     
     }
-
 }
