63394f0979c17fba246115e32ed31157b931473f
津崎 <g1244730@cse.kyoto-su.ac.jp>
2016-02-17 03:20:43 +0900

    Chapter1終了

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,24 +10,25 @@ import views.html.*;
 public class Application extends Controller {
     
     //フォーム内の内部クラス
-    public static class SampleForm{
+    public static class SampleForm {
         public String message;
+    }
         
     //ルートにアクセスした際のアクション
-    public static Result index(){
+    public Result index() {
         return ok(index.render("何か書いて",new Form(SampleForm.class)));
-        }
+    }
     
     //sendにアクセスした際のアクション
-    public static Result send(){
-      Form<SampleForm> f =   form(SampleForm.class).bindFormRequest();
-      if(!f.haserrors()){
-      }
+    public Result send() {
+      Form<SampleForm> f =   form(SampleForm.class).bindFromRequest();
+      if(!f.hasErrors()){
       SampleForm data = f.get();
-      String msg = "you typed" + data.message;
-      retun ok(index.render(msg.f));
-    } else {
-        return badrequest(index.render("ERROR",form(SampleForm.class));
-        }     
+      String msg = "you typed:" + data.message;
+      return ok(index.render(msg,f));
+      } else {
+        return badRequest(index.render("ERROR",form(SampleForm.class)));      
+      }
     }
 }
+
